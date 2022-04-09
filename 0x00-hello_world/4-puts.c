08e38b862d8b51b6153395cf80313dec3f2383de
ojogiggs <ojogiggs@gmail.com>
2022-04-09 05:39:58 -0700

    my low level c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+
+ * main- This is the main function
+
+ * Return: Value return should 0
+
+ */
+
+int main(void)
+
+{
+
+	char alx[] = "\"Programming is like building a multilingual puzzle";
+
+
+
+	puts(alx);
+
+	return (0);
+
+}
