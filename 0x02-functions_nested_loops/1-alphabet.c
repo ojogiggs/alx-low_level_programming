5e8b0e6a796322af2e89c610c1618f4ac66a6b4d
ojogiggs <ojogiggs@gmail.com>
2022-04-12 04:16:16 -0700

    filr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet-This is the function to print all the alphabet
+ * Return: it shouldnt return any value
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+
+	_putchar('\n');
+
+}
